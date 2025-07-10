58957d909c0ce32d267ed74f3834a7e04ec9337d
CleoKiama <cleophaskiama7@gmail.com>
2025-07-10 12:52:41 +0300

    feat(ui): Added css styling support.

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,6 @@ mod core;
 mod db;
 mod ui;
 
-const APP_ID: &str = "org.gtk_rs.habitron";
-
 fn main() {
     ui_main();
 }
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,9 +1,10 @@
+use anyhow::Context;
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, CssProvider, glib};
+use gtk::{Application, ApplicationWindow, Settings, glib};
 
 mod current_week;
 
-const APP_ID: &str = "org.gtk_rs.HelloWorld2";
+const APP_ID: &str = "org.gtk_rs.habitron";
 
 pub fn ui_main() -> glib::ExitCode {
     // Create a new application
@@ -17,11 +18,12 @@ pub fn ui_main() -> glib::ExitCode {
 }
 
 fn build_ui(app: &Application) {
-    load_css();
+    if let Err(err) = detect_and_apply_theme() {
+        eprintln!("something went wrong failed to load the css {}", err)
+    }
 
     let gtk_box = gtk::Box::builder()
-        .orientation(gtk::Orientation::Horizontal)
-        .width_request(12)
+        .orientation(gtk::Orientation::Vertical)
         .spacing(10)
         .css_classes(["day-container"])
         .build();
@@ -38,11 +40,41 @@ fn build_ui(app: &Application) {
         .child(&gtk_box)
         .build();
 
-    // Present window
     window.present();
 }
 
-fn load_css() {
-    let provider = CssProvider::new();
-    provider.load_from_path("./style.css");
+fn detect_and_apply_theme() -> Result<(), anyhow::Error> {
+    let settings =
+        Settings::default().ok_or_else(|| anyhow::anyhow!("Could not get default settings"))?;
+
+    let is_dark = settings.is_gtk_application_prefer_dark_theme();
+    if is_dark {
+        load_dark_theme_css()?
+    } else {
+        load_light_theme_css()?
+    }
+
+    Ok(())
+}
+
+fn load_dark_theme_css() -> Result<(), anyhow::Error> {
+    load_css_from_path("./style-dark.css")
+}
+
+fn load_light_theme_css() -> Result<(), anyhow::Error> {
+    load_css_from_path("./style-light.css")
+}
+
+fn load_css_from_path(path: &str) -> Result<(), anyhow::Error> {
+    let provider = gtk::CssProvider::new();
+    let css_file_path = std::path::Path::new(path);
+    provider.load_from_path(css_file_path);
+
+    gtk::style_context_add_provider_for_display(
+        &gtk::gdk::Display::default().with_context(|| "failed to load the css")?,
+        &provider,
+        gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+    );
+
+    Ok(())
 }
