9f5e659575f64ff68adf63d57315369308c04471
CleoKiama <cleophaskiama7@gmail.com>
2025-08-10 11:44:32 +0300

    refactor: use only the tokio types we need and the async_runtime from tauri

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod core;
-mod db;
-
 fn main() {
     habitron_lib::run();
-    let conn = db::Db::new("/tmp/habitron.db");
-
-    match conn {
-        Ok(db) => db.init_db(),
-        Err(e) => eprintln!("Failed to initialize database: {}", e),
-    }
 }
