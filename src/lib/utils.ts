0fd44f157838e409c0ade4a7102fcdbd9ee9b26f
CleoKiama <cleophaskiama7@gmail.com>
2025-09-05 09:24:46 +0300

    feat: added a tryCatch utility

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,27 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
 }
+
+type Success<T> = {
+	data: T;
+	error: null;
+};
+
+type Failure<E> = {
+	error: E;
+	data: null;
+};
+
+type Result<T, E> = Success<T> | Failure<E>;
+
+export const tryCatch = async <T, E>(promise: T): Promise<Result<T, E>> => {
+	try {
+		const data = await promise;
+		return { data, error: null };
+	} catch (error) {
+		return { data: null, error: error as E };
+	}
+};
